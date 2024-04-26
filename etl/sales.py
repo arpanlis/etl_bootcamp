0fa6d987a95af3c24df022f24552f86a59514da2
Arpan Koirala <arpan.koirala@yco.com.np>
2024-04-26 08:56:33 +0545

    feat: fact etl update

--- etl/sales.py
+++ etl/sales.py
@@ -56,10 +56,11 @@ class SalesETL(BaseETL):
     tgt_sales_agg_ddl = """
         ID_SK INT AUTOINCREMENT,
         MONTH VARCHAR,
-        STORE VARCHAR,
+        STORE_ID_SK INT,
         AMOUNT NUMBER,
         DISCOUNT NUMBER,
-        PRIMARY KEY (ID_SK)
+        PRIMARY KEY (ID_SK),
+        FOREIGN KEY (STORE_ID_SK) REFERENCES TGT.DWH_D_STORE_LU(ID_SK)
     """
 
     def __init__(self) -> None:
@@ -76,67 +77,56 @@ class SalesETL(BaseETL):
         conn = get_connection(schema="TGT")
         cur = conn.cursor()
 
-        # Insert new records
         cur.execute(
             f"""
-            INSERT INTO {self.TARGET_TABLE} (SOURCE_ID, STORE_ID_SK, PRODUCT_ID_SK, CUSTOMER_ID_SK, TRANSACTION_TIME, QUANTITY, AMOUNT, DISCOUNT)
-            SELECT SRC.ID, STR.ID_SK, PRD.ID_SK, CUS.ID_SK, SRC.TRANSACTION_TIME, SRC.QUANTITY, SRC.AMOUNT, SRC.DISCOUNT
-            FROM TMP.{self.TEMP_TABLE} SRC
-            LEFT JOIN TGT.DWH_D_STORE_LU STR ON SRC.STORE_ID = STR.SOURCE_ID
-            LEFT JOIN TGT.DWH_D_PRODUCT_LU PRD ON SRC.PRODUCT_ID = PRD.SOURCE_ID
-            LEFT JOIN TGT.DWH_D_CUSTOMER_LU CUS ON SRC.CUSTOMER_ID = CUS.SOURCE_ID
-            WHERE SRC.ID NOT IN (SELECT SOURCE_ID FROM {self.TARGET_TABLE})
-            """
-        )
-
-        cur.execute(
-            f"""
-            MERGE INTO {self.TARGET_TABLE} TGT
+            MERGE INTO TGT.{self.TARGET_TABLE} F
             USING (
-                SELECT SRC.ID, SRC.TRANSACTION_TIME, SRC.QUANTITY, SRC.AMOUNT, SRC.DISCOUNT,
-                    STR.ID_SK AS STORE_ID_SK, PRD.ID_SK AS PRODUCT_ID_SK, CUS.ID_SK AS CUSTOMER_ID_SK
-                FROM TMP.{self.TEMP_TABLE} SRC
-                LEFT JOIN TGT.DWH_D_STORE_LU STR ON SRC.STORE_ID = STR.SOURCE_ID AND STR.ACTIVE_FLAG = TRUE
-                LEFT JOIN TGT.DWH_D_PRODUCT_LU PRD ON SRC.PRODUCT_ID = PRD.SOURCE_ID AND PRD.ACTIVE_FLAG = TRUE
-                LEFT JOIN TGT.DWH_D_CUSTOMER_LU CUS ON SRC.CUSTOMER_ID = CUS.SOURCE_ID AND CUS.ACTIVE_FLAG = TRUE
+                SELECT
+                    S.ID AS SOURCE_ID,
+                    ST.ID_SK AS STORE_ID_SK,
+                    P.ID_SK AS PRODUCT_ID_SK,
+                    C.ID_SK AS CUSTOMER_ID_SK,
+                    S.TRANSACTION_TIME,
+                    S.QUANTITY,
+                    S.AMOUNT,
+                    S.DISCOUNT
+                FROM TMP.{self.TEMP_TABLE} S
+                JOIN TGT.DWH_D_STORE_LU ST ON ST.SOURCE_ID = S.STORE_ID AND ST.ACTIVE_FLAG = TRUE
+                JOIN TGT.DWH_D_PRODUCT_LU P ON P.SOURCE_ID = S.PRODUCT_ID AND P.ACTIVE_FLAG = TRUE
+                LEFT JOIN TGT.DWH_D_CUSTOMER_LU C ON C.SOURCE_ID = S.CUSTOMER_ID AND C.ACTIVE_FLAG = TRUE
             ) SRC
-            ON TGT.SOURCE_ID = SRC.ID AND TGT.ACTIVE_FLAG = TRUE
-            WHEN MATCHED AND (
-                TGT.TRANSACTION_TIME <> SRC.TRANSACTION_TIME
-                OR TGT.QUANTITY <> SRC.QUANTITY
-                OR TGT.AMOUNT <> SRC.AMOUNT
-                OR TGT.DISCOUNT <> SRC.DISCOUNT
-                OR TGT.STORE_ID_SK <> SRC.STORE_ID_SK
-                OR TGT.PRODUCT_ID_SK <> SRC.PRODUCT_ID_SK
-                OR TGT.CUSTOMER_ID_SK <> SRC.CUSTOMER_ID_SK
-            ) THEN
-            UPDATE
-            SET TGT.TRANSACTION_TIME = SRC.TRANSACTION_TIME,
-                TGT.QUANTITY = SRC.QUANTITY,
-                TGT.AMOUNT = SRC.AMOUNT,
-                TGT.DISCOUNT = SRC.DISCOUNT,
-                TGT.STORE_ID_SK = SRC.STORE_ID_SK,
-                TGT.PRODUCT_ID_SK = SRC.PRODUCT_ID_SK,
-                TGT.CUSTOMER_ID_SK = SRC.CUSTOMER_ID_SK
+            ON F.SOURCE_ID = SRC.SOURCE_ID
+            WHEN MATCHED THEN
+                UPDATE
+                SET F.STORE_ID_SK = SRC.STORE_ID_SK,
+                    F.PRODUCT_ID_SK = SRC.PRODUCT_ID_SK,
+                    F.CUSTOMER_ID_SK = SRC.CUSTOMER_ID_SK,
+                    F.TRANSACTION_TIME = SRC.TRANSACTION_TIME,
+                    F.QUANTITY = SRC.QUANTITY,
+                    F.AMOUNT = SRC.AMOUNT,
+                    F.DISCOUNT = SRC.DISCOUNT
+            WHEN NOT MATCHED THEN
+                INSERT (SOURCE_ID, STORE_ID_SK, PRODUCT_ID_SK, CUSTOMER_ID_SK, TRANSACTION_TIME, QUANTITY, AMOUNT, DISCOUNT)
+                VALUES (SRC.SOURCE_ID, SRC.STORE_ID_SK, SRC.PRODUCT_ID_SK, SRC.CUSTOMER_ID_SK, SRC.TRANSACTION_TIME, SRC.QUANTITY, SRC.AMOUNT, SRC.DISCOUNT);
             """
         )
 
-        # Clear aggregation table
-        cur.execute("TRUNCATE TABLE TGT.DWH_F_BHATBHATENI_AGG_SLS_PLC_MONTH_T")
-
-        # Generate sales aggregation
+        # Aggregate sales data by month and store
         cur.execute(
             f"""
-            INSERT INTO TGT.DWH_F_BHATBHATENI_AGG_SLS_PLC_MONTH_T (MONTH, STORE, AMOUNT, DISCOUNT)
-            SELECT 
-                TO_CHAR(TRANSACTION_TIME, 'YYYY-MM') AS MONTH,
-                STR.STORE_DESC AS STORE,
-                SUM(AMOUNT) AS AMOUNT,
-                SUM(DISCOUNT) AS DISCOUNT
-            FROM {self.TARGET_TABLE} SRC
-            JOIN TGT.DWH_D_STORE_LU STR ON SRC.STORE_ID_SK = STR.ID_SK
-            GROUP BY TO_CHAR(TRANSACTION_TIME, 'YYYY-MM'), STR.STORE_DESC
+            INSERT INTO TGT.DWH_F_BHATBHATENI_AGG_SLS_PLC_MONTH_T (MONTH, STORE_ID_SK, AMOUNT, DISCOUNT)
+            SELECT
+                TO_CHAR(S.TRANSACTION_TIME, 'YYYY-MM'),
+                ST.ID_SK as STORE_ID_SK,
+                SUM(S.AMOUNT),
+                SUM(S.DISCOUNT)
+            FROM TMP.{self.TEMP_TABLE} S
+            JOIN TGT.DWH_D_STORE_LU ST ON ST.SOURCE_ID = S.STORE_ID AND ST.ACTIVE_FLAG = TRUE
+            GROUP BY TO_CHAR(S.TRANSACTION_TIME, 'YYYY-MM'), ST.ID_SK;
             """
         )
-        print(f"Data has been loaded into {self.TARGET_TABLE}")
+
+        conn.commit()
         cur.close()
+
+        print(f"Data has been loaded into {self.TARGET_TABLE}")
